b41c28d42ca99371003d6cc35a0b6318d0e5e0e9
Sohee Moon <121299334+soheetech@users.noreply.github.com>
2023-01-21 07:46:36 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,12 @@ $(document).ready(function(){
       $('#menu-wrap').addClass('vh');
       $('#btn-menu').removeClass('open');
     }
-    $('#search-wrap').toggleClass('vh', function(){
-      if($('#search-wrap').hasClass('vh') == true){
-        $('header .btn-search').text('Search');
-      } else {
-        $('header .btn-search').text('Close');
-      }
-    });
+    $('#search-wrap').toggleClass('vh');
+    if($('#search-wrap').hasClass('vh') == true){
+      $('header .btn-search').text('Search');
+    } else {
+      $('header .btn-search').text('Close');
+    }
     return false;
   });
       
@@ -22,13 +21,12 @@ $(document).ready(function(){
       $('#search-wrap').addClass('vh');
       $('header .btn-search').text('Search');
     }
-    $('#menu-wrap').toggleClass('vh', function(){
-      if($('#menu-wrap').hasClass('vh') == true){
-        $('#btn-menu').removeClass('open');
-      } else {
-        $('#btn-menu').addClass('open');
-      }
-    });
+    $('#menu-wrap').toggleClass('vh');
+    if($('#menu-wrap').hasClass('vh') == true){
+      $('#btn-menu').removeClass('open');
+    } else {
+      $('#btn-menu').addClass('open');
+    }
     return false;
   });
   
