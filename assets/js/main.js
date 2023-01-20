5933d8885678d28da2586ca1718dcfe433a9e92c
Sohee Moon <121299334+soheetech@users.noreply.github.com>
2023-01-21 07:37:01 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
     if($('#menu-wrap').hasClass('vh') == false){
       $('#menu-wrap').addClass('vh');
     }
-    if($('#search-wrap').hasClass('vh') == false){
-      $('header .btn-search').text('Close');
-    } else {
+    if($('#search-wrap').hasClass('vh') == true){
       $('header .btn-search').text('Search');
+    } else {
+      $('header .btn-search').text('Close');
     }
     $('#search-wrap').toggleClass('vh');
     return false;
@@ -19,10 +19,10 @@ $(document).ready(function(){
     if($('#search-wrap').hasClass('vh') == false){
       $('#search-wrap').addClass('vh');
     }
-    if($('#menu-wrap').hasClass('vh') == false){
-      $('#btn-menu').addClass('open');
-    } else {
+    if($('#menu-wrap').hasClass('vh') == true){
       $('#btn-menu').removeClass('open');
+    } else {
+      $('#btn-menu').addClass('open');
     }
     $('#menu-wrap').toggleClass('vh');
     return false;
