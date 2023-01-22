d232b08076573eea5ed7edc8d3b4641afe14b6fd
Sohee Moon <121299334+soheetech@users.noreply.github.com>
2023-01-23 00:44:11 +0900

    Create tags.js

---
+++ assets/js/tags.js
@@ -0,0 +1,31 @@
+$("[data-tag]").click((e) => {
+  currentTag = e.target.dataset.tag;
+  filterByTagName(currentTag);
+  updateQueryString(currentTag);
+})
+
+function filterByTagName(tagName) {
+  $('.post-list > li.vh').removeClass('vh');
+  $('.post-list > li').each((index, elem) => {
+    if (!elem.hasAttribute(`data-${tagName}`)) {
+      $(elem).addClass('vh');
+    }
+  });
+  $('.item-tag').removeClass('selected');
+  $('.item-tag[data-tag=${tagName}]').addClass('selected');
+  return false;
+}
+  
+$(document).ready(function() {
+  let currentTag = "";
+  const queryTag = (new URLSearchParams(window.location.search)).get("tag");
+  if (queryTag) {
+    currentTag = queryTag;
+    filterByTagName(currentTag)
+  }
+});
+    
+function updateQueryString(tagName) {
+  const path = '${location.protocol}//${location.host}${location.pathname}?tag=${tagName}';
+  window.history.replaceState({ path }, '', path);
+}
